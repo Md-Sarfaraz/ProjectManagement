58041c49a25e43a29c322e6f613e610ecb07c483
Md Sarfaraz <sarfarazraj217@gmail.com>
2022-05-27 20:03:09 +0530

    updating

--- src/main/java/com/sarfaraz/management/controller/PublicController.java
+++ src/main/java/com/sarfaraz/management/controller/PublicController.java
@@ -51,7 +51,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping(value = { "", "/" })
+@RequestMapping(value = {  "/" })
 @RequiredArgsConstructor
 public class PublicController {
 
